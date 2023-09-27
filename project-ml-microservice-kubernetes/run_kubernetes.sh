47faa4253b7ea971e9b0f45a106bfbade67576a0
Ubuntu <ubuntu@ip-172-31-21-99.ec2.internal>
2023-09-27 03:25:59 +0000

    Thêm thư mục project-ml-microservice-kubernetes vào dự án

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="trungld6/mircoproject:v1.0.0"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+ kubectl run mircoproject \
+    --image=trungld6/mircoproject:v1.0.0 \
+    --port=80 --labels app=mircoproject
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mircoproject 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="trungld6/mircoproject:v1.0.0"
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag mircoproject $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
